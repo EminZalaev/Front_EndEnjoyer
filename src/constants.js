cc920a04fac397b2c75d66caaa24b5c5b7979a86
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-02-10 00:01:03 +0300

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const firstName = 'HR'
+const secondName = 'вакансию'
+const dz_name = 'Рекрутинг'
+const student = 'Залаев Мухаммад-Эмин'
+const first_form_first = 'имя HR'
+const second_form_first = 'название'
+const second_form_second = 'компанию'
+const second_form_third = ''
+const domain = 'http://localhost:8000/api'
+
+export {firstName, secondName, domain, dz_name, student, first_form_first, second_form_second, second_form_third, second_form_first}
